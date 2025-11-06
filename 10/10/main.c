d8cd5b90da6b6a7559aea415a6968829c233c2c7
yun9017 <yun9017ie@naver.com>
2025-11-06 15:17:50 +0900

    1

---
+++ 10/10/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  10
+//
+//  Created by 한서윤 on 11/6/25.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    char code1 = 'A';
+    int code2 = 65;
+    
+    printf("code1 = %c,(%d)\n",code1, code1);
+    printf("code2 = %c,(%d)\n",code2, code2);
+    return 0;
+}
