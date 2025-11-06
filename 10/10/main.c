a487f1a30b98c50ee687e4827429786363e070ad
yun9017 <yun9017ie@naver.com>
2025-11-06 15:45:22 +0900

    5

--- 10/10/main.c
+++ 10/10/main.c
@@ -8,9 +8,25 @@
 #include <stdio.h>
 #include <string.h>
 
-void main(void) {
-    char str[30] = "happy C programming";
+int main(void) {
+    char input[100];
+    FILE* fp;
+    int i;
     
-    printf("string %s length : %i\n",str, strlen(str));
+    //fopen
+    fp = fopen("sample.txt","w");
+    
+    for(i=0;i<3;i++)
+    {
+        //fprintf
+        printf("input a word:");
+        scanf("%s", input); //문자 여러개 배열에 저장, &쓰지 않고 배열 이름 그대로
+        fprintf(fp,"%s\n",input);
+    }
+    //fclose
+    fclose(fp);
+    
+    return 0;
+    
     }
 
